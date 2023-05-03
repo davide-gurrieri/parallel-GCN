2ce7141a6320ae7da20e7e646c31e95cd3cb250f
Davide Gurrieri <gurrieri99@gmail.com>
2023-05-03 11:25:03 +0200

    added some comments

--- sequential/include/optim.h
+++ sequential/include/optim.h
@@ -1,29 +1,32 @@
 #ifndef OPTIM_H
-#include <vector>
-#include <utility>
 #include "variable.h"
+#include <utility>
+#include <vector>
+
 
 struct AdamParams {
-    float lr, beta1, beta2, eps, weight_decay;
-    static AdamParams get_default();
+  float lr, beta1, beta2, eps, weight_decay;
+  static AdamParams get_default();
 };
 
 struct AdamVariable {
-    std::vector<float> *data, *grad, m, v;
-    bool decay;
+  std::vector<float> *data, *grad, m, v;
+  bool decay;
+
 public:
-    int size();
-    AdamVariable(Variable*, bool);
+  int size();
+  AdamVariable(Variable *, bool);
 };
 
 class Adam {
-    AdamParams params;
-    int step_count;
-    std::vector<AdamVariable> vars;
+  AdamParams params;
+  int step_count;
+  std::vector<AdamVariable> vars;
+
 public:
-    Adam() {}
-    Adam(std::vector<std::pair<Variable*, bool>> vars, AdamParams params);
-    void step();
+  Adam() {}
+  Adam(std::vector<std::pair<Variable *, bool>> vars, AdamParams params);
+  void step();
 };
 
 #define OPTIM_H
--- sequential/include/rand.h
+++ sequential/include/rand.h
@@ -1,20 +1,22 @@
 #ifndef RAND_H
-#include <cstdlib>
-#include <cstdint>
 #include <assert.h>
+#include <cstdint>
+#include <cstdlib>
 
-#define MY_RAND_MAX 0x7fffffff
+
+#define MY_RAND_MAX                                                            \
+  0x7fffffff // is the maximum value that can be represented by a 32-bit signed
+             // integer in hexadecimal notation. In decimal notation, it is
+             // equal to 2,147,483,647.
 
 void init_rand_state();
 const int CACHE_LINE_SIZE = 64;
 const int MAX_NUM_THREADS = 64;
 
-uint32_t xorshift128plus(uint64_t* state);
-
+uint32_t xorshift128plus(uint64_t *state);
 
 extern uint64_t rand_state[2];
 #define RAND() xorshift128plus(&rand_state[0])
-
 
 #define RAND_H
 #endif
